a05b1c50d994b29ccfc4d8beafd9ba6c5a7b15da
runningxiaobai <runningtoilet@hotmail.com>
2025-08-21 11:25:54 +0800

    Beta v0.33

---
+++ script/mp3play.js
@@ -0,0 +1,13 @@
+var urlParams = new URLSearchParams(window.location.search);
+var album = urlParams.get("album");
+var shuffle = urlParams.get("shuffle");
+var favorite = urlParams.get("favorite");
+
+var bgmHR10 = new Array();
+var titleHR10 = new Array();
+
+bgmHR10[0] = "2154797765";
+titleHR10[0] = "七夕坂に朝が来る";
+bgmHR10[1] = "2154796502";
+titleHR10[1] = "不等式のティンカーベル";
+
